67bbaa019c989ecca00d081b5b2ac2770a1cda8f
Bipul-Ch-Roy <kumarroy.kb.kb@gmail.com>
2021-09-18 00:12:30 +0600

    first commit

---
+++ js/script.js
@@ -0,0 +1,17 @@
+$(document).ready(function(){
+	$('.window.start').click(function(){
+		$('.windows-menu').toggleClass('bondho');
+	});
+});
+$(document).ready(function(){
+	$('.my-computer').click(function(){
+		$('.ths-pc').toggleClass('bipul');
+	});
+});
+$(document).ready(function(){
+	$('.boxx').click(function(){
+		$('.bbig-box').toggleClass('cl');
+	});
+});
+
+new WOW().init();
